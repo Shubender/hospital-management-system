8fe2c4bd242fc09925bfefe0b1dbc644b83c7723
shubender <2andrey.verbitskiy@gmail.com>
2023-05-12 11:57:37 +0200

    remove blocks to different files

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -57,11 +57,13 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text" id="icon-addon1"><span class="mai-search"></span></span>
                     </div>
-                    <input type="text" class="form-control" placeholder="Enter keyword.." aria-label="Username" aria-describedby="icon-addon1">
+                    <input type="text" class="form-control" placeholder="Enter keyword.." aria-label="Username"
+                           aria-describedby="icon-addon1">
                 </div>
             </form>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport" aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport"
+                    aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -84,19 +86,19 @@
                     </li>
 
                     @if(Route::has('login'))
-                    @auth
-                        <x-app-layout>
-{{--                            <h1>test</h1>--}}
-                        </x-app-layout>
-                    @else
+                        @auth
+                            <x-app-layout>
+                                {{--                            <h1>test</h1>--}}
+                            </x-app-layout>
+                        @else
 
-                    <li class="nav-item">
-                        <a class="btn btn-primary ml-lg-3" href="{{route('login')}}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-primary ml-lg-3" href="{{route('register')}}">Register</a>
-                    </li>
-                    @endauth
+                            <li class="nav-item">
+                                <a class="btn btn-primary ml-lg-3" href="{{route('login')}}">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="btn btn-primary ml-lg-3" href="{{route('register')}}">Register</a>
+                            </li>
+                        @endauth
                     @endif
                 </ul>
             </div> <!-- .navbar-collapse -->
@@ -152,7 +154,11 @@
             <div class="row align-items-center">
                 <div class="col-lg-6 py-3 wow fadeInUp">
                     <h1>Welcome to Your Health <br> Center</h1>
-                    <p class="text-grey mb-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Accusantium aperiam earum ipsa eius, inventore nemo labore eaque porro consequatur ex aspernatur. Explicabo, excepturi accusantium! Placeat voluptates esse ut optio facilis!</p>
+                    <p class="text-grey mb-4">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos
+                        et accusam et justo duo dolores et ea rebum. Accusantium aperiam earum ipsa eius, inventore nemo
+                        labore eaque porro consequatur ex aspernatur. Explicabo, excepturi accusantium! Placeat
+                        voluptates esse ut optio facilis!</p>
                     <a href="about.html" class="btn btn-primary">Learn More</a>
                 </div>
                 <div class="col-lg-6 wow fadeInRight" data-wow-delay="400ms">
@@ -165,228 +171,11 @@
     </div> <!-- .bg-light -->
 </div> <!-- .bg-light -->
 
-<div class="page-section">
-    <div class="container">
-        <h1 class="text-center mb-5 wow fadeInUp">Our Doctors</h1>
+@include('user.doctor');
 
-        <div class="owl-carousel wow fadeInUp" id="doctorSlideshow">
-            <div class="item">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img src="../assets/img/doctors/doctor_1.jpg" alt="">
-                        <div class="meta">
-                            <a href="#"><span class="mai-call"></span></a>
-                            <a href="#"><span class="mai-logo-whatsapp"></span></a>
-                        </div>
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Dr. Stein Albert</p>
-                        <span class="text-sm text-grey">Cardiology</span>
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img src="../assets/img/doctors/doctor_2.jpg" alt="">
-                        <div class="meta">
-                            <a href="#"><span class="mai-call"></span></a>
-                            <a href="#"><span class="mai-logo-whatsapp"></span></a>
-                        </div>
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Dr. Alexa Melvin</p>
-                        <span class="text-sm text-grey">Dental</span>
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img src="../assets/img/doctors/doctor_3.jpg" alt="">
-                        <div class="meta">
-                            <a href="#"><span class="mai-call"></span></a>
-                            <a href="#"><span class="mai-logo-whatsapp"></span></a>
-                        </div>
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Dr. Rebecca Steffany</p>
-                        <span class="text-sm text-grey">General Health</span>
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img src="../assets/img/doctors/doctor_3.jpg" alt="">
-                        <div class="meta">
-                            <a href="#"><span class="mai-call"></span></a>
-                            <a href="#"><span class="mai-logo-whatsapp"></span></a>
-                        </div>
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Dr. Rebecca Steffany</p>
-                        <span class="text-sm text-grey">General Health</span>
-                    </div>
-                </div>
-            </div>
-            <div class="item">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img src="../assets/img/doctors/doctor_3.jpg" alt="">
-                        <div class="meta">
-                            <a href="#"><span class="mai-call"></span></a>
-                            <a href="#"><span class="mai-logo-whatsapp"></span></a>
-                        </div>
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Dr. Rebecca Steffany</p>
-                        <span class="text-sm text-grey">General Health</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+@include('user.latest');
 
-<div class="page-section bg-light">
-    <div class="container">
-        <h1 class="text-center wow fadeInUp">Latest News</h1>
-        <div class="row mt-5">
-            <div class="col-lg-4 py-2 wow zoomIn">
-                <div class="card-blog">
-                    <div class="header">
-                        <div class="post-category">
-                            <a href="#">Covid19</a>
-                        </div>
-                        <a href="blog-details.html" class="post-thumb">
-                            <img src="../assets/img/blog/blog_1.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="body">
-                        <h5 class="post-title"><a href="blog-details.html">List of Countries without Coronavirus case</a></h5>
-                        <div class="site-info">
-                            <div class="avatar mr-2">
-                                <div class="avatar-img">
-                                    <img src="../assets/img/person/person_1.jpg" alt="">
-                                </div>
-                                <span>Roger Adams</span>
-                            </div>
-                            <span class="mai-time"></span> 1 week ago
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 py-2 wow zoomIn">
-                <div class="card-blog">
-                    <div class="header">
-                        <div class="post-category">
-                            <a href="#">Covid19</a>
-                        </div>
-                        <a href="blog-details.html" class="post-thumb">
-                            <img src="../assets/img/blog/blog_2.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="body">
-                        <h5 class="post-title"><a href="blog-details.html">Recovery Room: News beyond the pandemic</a></h5>
-                        <div class="site-info">
-                            <div class="avatar mr-2">
-                                <div class="avatar-img">
-                                    <img src="../assets/img/person/person_1.jpg" alt="">
-                                </div>
-                                <span>Roger Adams</span>
-                            </div>
-                            <span class="mai-time"></span> 4 weeks ago
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 py-2 wow zoomIn">
-                <div class="card-blog">
-                    <div class="header">
-                        <div class="post-category">
-                            <a href="#">Covid19</a>
-                        </div>
-                        <a href="blog-details.html" class="post-thumb">
-                            <img src="../assets/img/blog/blog_3.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="body">
-                        <h5 class="post-title"><a href="blog-details.html">What is the impact of eating too much sugar?</a></h5>
-                        <div class="site-info">
-                            <div class="avatar mr-2">
-                                <div class="avatar-img">
-                                    <img src="../assets/img/person/person_2.jpg" alt="">
-                                </div>
-                                <span>Diego Simmons</span>
-                            </div>
-                            <span class="mai-time"></span> 2 months ago
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 text-center mt-4 wow zoomIn">
-                <a href="blog.html" class="btn btn-primary">Read More</a>
-            </div>
-
-        </div>
-    </div>
-</div> <!-- .page-section -->
-
-<div class="page-section">
-    <div class="container">
-        <h1 class="text-center wow fadeInUp">Make an Appointment</h1>
-
-        <form class="main-form">
-            <div class="row mt-5 ">
-                <div class="col-12 col-sm-6 py-2 wow fadeInLeft">
-                    <input type="text" class="form-control" placeholder="Full name">
-                </div>
-                <div class="col-12 col-sm-6 py-2 wow fadeInRight">
-                    <input type="text" class="form-control" placeholder="Email address..">
-                </div>
-                <div class="col-12 col-sm-6 py-2 wow fadeInLeft" data-wow-delay="300ms">
-                    <input type="date" class="form-control">
-                </div>
-                <div class="col-12 col-sm-6 py-2 wow fadeInRight" data-wow-delay="300ms">
-                    <select name="departement" id="departement" class="custom-select">
-                        <option value="general">General Health</option>
-                        <option value="cardiology">Cardiology</option>
-                        <option value="dental">Dental</option>
-                        <option value="neurology">Neurology</option>
-                        <option value="orthopaedics">Orthopaedics</option>
-                    </select>
-                </div>
-                <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
-                    <input type="text" class="form-control" placeholder="Number..">
-                </div>
-                <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
-                    <textarea name="message" id="message" class="form-control" rows="6" placeholder="Enter message.."></textarea>
-                </div>
-            </div>
-
-            <button type="submit" class="btn btn-primary mt-3 wow zoomIn">Submit Request</button>
-        </form>
-    </div>
-</div> <!-- .page-section -->
-
-<div class="page-section banner-home bg-image" style="background-image: url(../assets/img/banner-pattern.svg);">
-    <div class="container py-5 py-lg-0">
-        <div class="row align-items-center">
-            <div class="col-lg-4 wow zoomIn">
-                <div class="img-banner d-none d-lg-block">
-                    <img src="../assets/img/mobile_app.png" alt="">
-                </div>
-            </div>
-            <div class="col-lg-8 wow fadeInRight">
-                <h1 class="font-weight-normal mb-3">Get easy access of all features using One Health Application</h1>
-                <a href="#"><img src="../assets/img/google_play.svg" alt=""></a>
-                <a href="#" class="ml-2"><img src="../assets/img/app_store.svg" alt=""></a>
-            </div>
-        </div>
-    </div>
-</div> <!-- .banner-home -->
+@include('user.appointment');
 
 <footer class="page-footer">
     <div class="container">
@@ -436,7 +225,8 @@
 
         <hr>
 
-        <p id="copyright">Copyright &copy; 2020 <a href="https://macodeid.com/" target="_blank">MACode ID</a>. All right reserved</p>
+        <p id="copyright">Copyright &copy; 2020 <a href="https://macodeid.com/" target="_blank">MACode ID</a>. All right
+            reserved</p>
     </div>
 </footer>
 
